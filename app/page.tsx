0a55ef12bc174a457335f6d83fff784cfbf42f84
kislay-gupta <kislaygupta007@gmail.com>
2024-03-18 23:13:21 +0530

    feat: updated coming soon page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
+import ComingSoon from "@/components/shared/ComingSoon";
 
 export default function Home() {
   return (
     <main className="">
-      <section>
-        <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24">
-          <div className="flex flex-col w-full mb-12 text-center">
-            <h1 className="max-w-5xl text-2xl font-bold leading-none tracking-tighter text-neutral-600 md:text-5xl lg:text-6xl lg:max-w-7xl">
-              Coming Soon
-            </h1>
-          </div>
-        </div>
-      </section>
+      <ComingSoon />
     </main>
   );
 }
--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,17 +1,19 @@
+import ComingSoon from "@/components/shared/ComingSoon";
 import { getAllUser } from "@/lib/action/user.action";
 import Link from "next/link";
 import React from "react";
 
 const Page = async () => {
-  const results = await getAllUser();
-  
+  // const results = await getAllUser();
+
   return (
-    <div className="flex flex-col gap-2 mt-11">
-      {results.users.map((data) => (
+    <div className="">
+      <ComingSoon />
+      {/* {results.users.map((data) => (
         <Link href={`/user/${data._id}`} key={data._id}>
           {data.fullName}
         </Link>
-      ))}
+      ))} */}
     </div>
   );
 };
